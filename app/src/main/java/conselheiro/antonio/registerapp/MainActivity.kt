37967d1b60c07308414c87fbd6c4a74c25c6af2e
Renan <mirandarenan.e@gmail.com>
2018-10-14 23:41:55 -0300

    Learning Shared Preferences

--- app/src/main/java/conselheiro/antonio/registerapp/MainActivity.kt
+++ app/src/main/java/conselheiro/antonio/registerapp/MainActivity.kt
@@ -6,13 +6,26 @@ import android.os.Bundle
 import android.widget.Toast
 import conselheiro.antonio.registerapp.model.LoginResponse
 import conselheiro.antonio.registerapp.model.User
+import conselheiro.antonio.registerapp.ui.activity.Home
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var tokenLoginValue : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val session = SessionManager(applicationContext)
+
+        if (session.isLoggedIn()){
+            var i: Intent = Intent(applicationContext, Home::class.java)
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(i)
+            finish()
+        }
 
         getIn.setOnClickListener{
             val usrname  = username.text
@@ -21,8 +34,9 @@ class MainActivity : AppCompatActivity() {
             val user = User(usrname.toString(), pwd.toString())
 
             LoginWebClient().login(user, object : LoginResponse<User> {
-                override fun success(token: User) {
-                    Toast.makeText(this@MainActivity, token.token, Toast.LENGTH_LONG).show()
+                override fun success(user: User) {
+                    session.createLoginSession(user.email, user.token)
+                    Toast.makeText(this@MainActivity, user.email, Toast.LENGTH_LONG).show()
                 }
             })
         }
---
+++ app/src/main/java/conselheiro/antonio/registerapp/SessionManager.kt
@@ -0,0 +1,61 @@
+package conselheiro.antonio.registerapp
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+
+class SessionManager {
+    lateinit var pref: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
+    lateinit var con : Context
+    var PRIVATE_MODE: Int = 0
+
+    constructor(con : Context){
+        this.con = con
+        pref = con.getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+        editor = pref.edit()
+    }
+
+    companion object {
+        val PREF_NAME: String = "LoginData"
+        val IS_LOGIN: String = "isLoggedIn"
+        val KEY_EMAIL: String = "email"
+        val KEY_TOKEN: String = "token"
+    }
+
+    fun createLoginSession(email : String, token: String){
+        editor.putBoolean(IS_LOGIN, true)
+        editor.putString(KEY_EMAIL, email)
+        editor.putString(KEY_TOKEN, token)
+        editor.commit()
+    }
+
+    fun getUserDetails(){
+        var user: Map<String, String> = HashMap<String, String>()
+        (user as HashMap).put(KEY_EMAIL, pref.getString(KEY_EMAIL, null))
+        (user as HashMap).put(KEY_TOKEN, pref.getString(KEY_TOKEN, null))
+    }
+
+    fun LogoutUser(){
+        editor.clear()
+        editor.commit()
+
+        var i: Intent = Intent(con, MainActivity::class.java)
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        con.startActivity(i)
+    }
+
+    fun isLoggedIn(): Boolean{
+        return pref.getBoolean(IS_LOGIN, false)
+    }
+
+    fun checkLogin(){
+        if (!isLoggedIn()){
+            var i: Intent = Intent(con, MainActivity::class.java)
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            con.startActivity(i)
+        }
+    }
+}
